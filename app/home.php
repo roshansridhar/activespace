4ba3b48b1380d1ebfe47429af23601bca8cc64d7
RustyFox <fiona.sequeira891@gmail.com>
2017-04-10 04:53:15 -0400

    Update home.php
    Search implementation added to homepage. Feed pending.

--- app/home.php
+++ app/home.php
@@ -1,4 +1,6 @@
 <?php
+  $Invalid_search='';
+  $input='';
   include('includes/db_connect.php');  
   
   session_start();
@@ -15,8 +17,8 @@
     <title>ActiveSpace: HomePage</title>
     <link rel="stylesheet" type="text/css" href="assets/styles/style.css">
     <meta http-equiv="Content-Type" content="text/html"; charset="utf-8" />
-</head>
 <style type="text/css">
+ 
  h4 { 
   color: #FFCA28; 
   text-shadow: 10px 1 white, 1 10px white, 15px 1 white, 1 20px white; 
@@ -37,52 +39,133 @@
   text-shadow: 1px 1px 2px #082b94;
 }
 
+.tabs_button{
+  margin: 0 0px -5px; 
+  float: left;
+  margin-top: -5px; 
+}
+
+.search {
+  float: right;
+  display: inline-block;
+  color: white;
+  background-color: rgba(0, 0, 0, .70);
+  text-align: center;
+  padding: 5px 5px;
+  text-decoration: none;
+  font-size: 20px;
+  margin-top: 0px;
+  border: groove;
+  margin-right: 20px;
+  vertical-align: 10px;
+}
+
+input[type="text"]{
+    width: 100px;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    font-size: 15px;
+    color: white;
+    background-color: black;
+    padding: 10px;
+    margin-top: -10px;
+  }
+
+
+input[type="radio"] {
+  display: inline-block;
+  font-size: 5px;  
+   width: 10px;  
+}
+
+input[type="submitbutton"] {
+  display: block;
+  font-size: 5px;  
+   width: 10px;  
+}
+
+
 </style>
 </head>
 <body>
+
 <div align="left"><h4> ActiveSpace </h4></div>
-<div class="w3-container">
-  <h2>H o m e p a g e</h2>
-  <p></p>
-
-  <div class="tab_button">
-    <button class="tab_button" onclick="existinguser.php?variable_user=$_GET["variable"]>My Live Feed</button>
-    <button class="tabs_button" onclick="profile.php?variable1=">My Profile</button>
-    <button class="tabs_button" onclick="diary.php?variable1=">My Diary Entry</button>
-    <button class="tabs_button" onclick="photos.php?variable1=">My Photos</button>
+    <div class="tabs_button">
+    <button class="tabs_button" onclick="home.php?variable1=".$_SESSION['EMAIL_ID']>My Live Feed</button>
+    <button class="tabs_button" onclick="profile.php?variable1=".$_SESSION['EMAIL_ID']>My Profile</button>
+    <button class="tabs_button" onclick="diary.php?variable1=".$_SESSION['EMAIL_ID']>My Diary Entry</button>
+    <button class="tabs_button" onclick="photos.php?variable1=".$_SESSION['EMAIL_ID']>My Photos</button>
   </div>
-  
-  <div id="London" class="tab_button">
-    <h2>London</h2>
-    <p>London is the capital city of England.</p>
+  <div align="center">
+  <form class="search" action="home.php" align="center" method="post">       
+            <p> Search network </p>
+            <input type="text" class="form-control" name="Name" placeholder="Enter name" />
+            <br>
+            <input type="radio" name="Association" value="1" checked /> Friend<br>
+            <input type="radio" name="Association" value="2"> Friend of Friend<br>
+            <input type="radio" name="Association" value="3"> Everyone <br>
+            <div align="right">
+            <input type="submit" name="submitbutton" value="Submit">
+            </div>
+  </form>
   </div>
-
-  <div id="Paris" class="w3-container w3-border city" style="display:none">
-    <h2>Paris</h2>
-    <p>Paris is the capital of France.</p> 
-  </div>
-
-  <div id="Tokyo" class="w3-container w3-border city" style="display:none">
-    <h2>Tokyo</h2>
-    <p>Tokyo is the capital of Japan.</p>
-  </div>
-</div>
-
+</body>
+</html>
 
 
 <?php 
+  if(isset($_GET["variable1"])){
+    echo "Welcome to the Live Feed";
+  }
+  
+  if(isset($_POST["submitbutton"])&&(isset($_GET["variable1'"]))) { 
+    $input=$_POST['Association'];    
+    
+    if(empty($_POST['Name'])) {
+        $Invalid_Search= 'No Name or Association input';
+    }
 
-if(isset($_GET["variable1"])){
-$db = pg_connect("host=localhost port=5433 dbname=activespace user=postgres password=test");
-$query = "SELECT column_name FROM information_schema.columns where table_schema = 'public' and table_name = '".$_GET['variable1']."';";
-$result = pg_query($query); 
-while($row = pg_fetch_row($result)){
-  echo '<a href="insert.php?variable2='.$row[0].'">'.$row[0].'</a>';
-  echo '<br>';
-};
-;
-}
+    if ($input=="1"){
+        $id= "select user_id from userinfo where email_id='".$_GET["variable1"]."';";
+        $query = "select username, picture_medium, first_name, last_name from userinfo, friendrelation where userinfo.user_id=friendrelation.user_two_id and friendship_status=2 and user_one_id='".$id."' and first_name like '%".$_POST['Name']."%'
+        UNION
+        select username, picture_medium first_name, last_name from userinfo, friendrelation where userinfo.user_id=friendrelation.user_one_id and friendship_status=2 and user_two_id='".$id."'and first_name like '%".$_POST['Name']."%';";
+        $result = pg_query($query); 
+        while($result){
+        echo '<a href="search.php?variable_search='.$row[0].'">'.$row[0].$row[1].$row[2].$row[3].'</a>';
+        echo '<br>';
+      }
+    }
+
+    if ($input=="2"){
+        $id= "select user_id from userinfo where email_id='".$_GET["variable1"]."';";
+        $query = "With mutual_user as
+        (select user_two_id from friendrelation where friendship_status=2 and user_one_id in
+        (select user_two_id from friendrelation where friendship_status=2 and user_one_id='".$id."' UNION select user_one_id from friendrelation where friendship_status=2 and user_two_id='".$id."')
+        UNION
+        select user_one_id from friendrelation where friendship_status=2 and user_two_id in
+          (select user_two_id from friendrelation where friendship_status=2 and user_one_id='".$id."' UNION select user_one_id from friendrelation where friendship_status=2 and user_two_id='".$id."'))
+
+        select username, picture_medium, first_name, last_name from userinfo,mutual_user where mutual_user.user_two_id=userinfo.user_id and user_two_id not in(
+          select user_two_id from friendrelation where friendship_status=2 and user_one_id='".$id."' UNION
+          select user_one_id from friendrelation where friendship_status=2 and user_two_id='".$id."' UNION
+          select user_id from userinfo where user_id='".$id."') and first_name like '%".$_POST['Name']."%';";
+        $result = pg_query($query); 
+        while($result){
+        echo '<a href="search.php?variable_search='.$row[0].'">'.$row[0].$row[1].$row[2].$row[3].'</a>';
+        echo '<br>';
+        }
+      }
+
+   if ($input=="3"){
+      $id= "select user_id from userinfo where email_id='".$_GET["variable1"]."';";
+      $query = " select username, picture_medium, first_name, last_name from userinfo where first_name like '".$_POST['Name']."';"; 
+       $result = pg_query($query); 
+        while($result){
+        echo '<a href="search.php?variable_search='.$row[0].'">'.$row[0].$row[1].$row[2].$row[3].'</a>';
+        echo '<br>';
+      }
+    }
+  }
 ?>
 
-</body>
-</html>
