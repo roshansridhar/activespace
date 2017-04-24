6031e37c858e3baaa9c28816508dd718c068d784
RustyFox <fiona.sequeira891@gmail.com>
2017-04-23 21:39:59 -0400

    Posts upload to everyone/friends implemented completely

--- app/upload_post.php
+++ app/upload_post.php
@@ -24,20 +24,19 @@
     <?php include('../includes/navbar.php'); ?>
 
     <div class="container">
-        <div class="container">
-        <p align="center">Share a POST with everyone today!</p>
-        <form action="upload_diary.php" method="post" enctype="multipart/form-data">
-        <br>
-        <p align="center"> What's on your mind <br>
-        <input type="text" class="description" name="title" placeholder=" " />
+    <p align="center">Share a POST with everyone today!</p>
+    <form action="upload_post.php" method="post">
+    <p align="center"> What's on your mind <br>
+    
+    <input type="text" class="description" name="description" placeholder=" " />
         
-        <p align="center"> To broadcast this post to everyone in your network, hit EVERYONE! else hit Specify Friend<br>
-          <input type="radio" name="Association" value="1" checked /> Everyone
-          <input type="radio" name="Association" value="2"> Pick a Friend
+    <p align="center"> To broadcast this post to everyone in your network, hit EVERYONE! else hit Specify Friend<br>
+    <input type="radio" name="Association" value="1" checked /> Everyone
+    <input type="radio" name="Association" value="2"> Pick a Friend
         
-        <select name="pick_friend">
-          <?php
-            $id= "select user_id from userinfo where email_id like '".$_SESSION['EmailID']."';";
+    <select name="pick_friend">
+      <?php
+       $id= "select user_id from userinfo where email_id like '".$_SESSION['EmailID']."';";
             $result1=pg_query($id);
             $id_op=pg_fetch_row($result1);
             
@@ -50,7 +49,7 @@
               echo '<option value ='.$row[0].'>'.$row[1].', '.$row[2].' '.$row[3].'</option>';
               }
             echo '</select>';
-          ?>
+      ?>
         <br><br>
        <input align="center" type="image" alt="Submit" src="img_submit.png" name="submitbutton" value="Submit" width="45" height="45">
         <br>
@@ -58,10 +57,33 @@
 
         </p>
         
-        </form>
+      </form>
         
-         <?php
-         if(isset($_SESSION['EmailID'])){
+      <?php
+         if(isset($_SESSION['EmailID'])&&(isset($_POST['submitbutton']))) {
+          $id= "select user_id from userinfo where email_id like '".$_SESSION['EmailID']."';";
+          $result1=pg_query($id);
+          $id_op=pg_fetch_row($result1);
+
+          // Gets description from POST 
+          $description = $_POST['description'];
+                        // Gets photo name
+          $option = $_POST['Association'];
+          $specific_user= $_POST['pick_friend'];
+
+          if($option=='1'){
+            $query = "select upload_posts('".$id_op[0]."','".$id_op[0]."','".$description."');";
+            $result = pg_query($query);
+            echo "<p>Post has been successfully shared with everyone! <br><br> Click on POSTS in navigation bar to check out the latest uploads!</p>";
+          }
+          if($option=='2'){
+            $query = "select upload_posts('".$id_op[0]."','".$specific_user."','".$description."');";
+            $result = pg_query($query);
+            $userquery= "select username from userinfo where user_id ='".$specific_user."';";
+            $result1=pg_query($userquery);
+            $userid=pg_fetch_row($result1);
+            echo "<p>Post has been successfully shared with <a href=search.php?variable_search=".$userid[0].">".$userid[0]."</a> !! <br><br> Click on POSTS in navigation bar to check out the latest uploads!</P>";
+          }
 
           }   
         ?>
